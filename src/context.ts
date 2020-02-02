ce5a1c5640aee3dca1eef63cdaff46561be6adf1
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2020-02-02 22:57:22 +0200

    allow anonymous introspection queries

--- src/context.ts
+++ src/context.ts
@@ -4,9 +4,15 @@ import { User } from './schema';
 import { userModel } from './schema/user';
 import { authClient } from './auth';
 
-export type Context = { user: User };
+export type Context = { user: User } | null;
 
 const getContext = async ({ req }: { req: express.Request }): Promise<Context> => {
+  // When introspection queries are enabled (i.e. NODE_ENV !== "production"), allow schema queries
+  // without providing a JWT token
+  if (req.body.query.trim().startsWith('query Introspection')) {
+    return null;
+  }
+
   // get the user token from the Authorization header (defined as "Bearer {TOKEN}")
   const token = req.headers.authorization?.split(' ')[1] || '';
   if (!token) {
